762bc4e8d7e706b4a689aeedaac5368e679f391a
addwon <addwon@gmail.com>
2018-02-10 14:51:47 -0600

    With Cloudinary

--- src/main/java/com/week3challenge/jbc_library/HomeController.java
+++ src/main/java/com/week3challenge/jbc_library/HomeController.java
@@ -1,5 +1,6 @@
 package com.week3challenge.jbc_library;
 
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 //import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 //import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 //import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -21,11 +23,15 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.Map;
 
 @Controller
 public class HomeController {
     @Autowired
     BookRepository bookRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
 
     String bookBorrowMessage="";
     String bookRetrunMessage="";
@@ -51,9 +57,21 @@ public class HomeController {
         return "add";
     }
     @PostMapping("/process")
-    public String addBookForm(@Valid @ModelAttribute("book") Book book, BindingResult result,
+    public String addBookForm(@Valid @ModelAttribute("book") Book book,@RequestParam("file")MultipartFile file, BindingResult result,
                               RedirectAttributes redirectAttributes){
         if(result.hasErrors()){
+            return "add";
+        }
+        if(file.isEmpty()){
+            return "add";
+        }
+        try{
+            Map uploadResult=cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype","auto"));
+            book.setImagePath(uploadResult.get("url").toString());
+            //actorRepository.save(actor);
+        }catch (IOException e){
+            e.printStackTrace();
             return "add";
         }
         book.setAvailability("Yes");
@@ -70,7 +88,7 @@ public class HomeController {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         book.setTimeStamp(dateFormat.format(date).toString());
-        
+
         bookBorrowMessage="\""+book.getTitle()+"\""+" is borrowed";
 
         redirectAttributes.addFlashAttribute("message1", bookBorrowMessage);
